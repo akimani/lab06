5fa7ee13dbae48b2946e9702f5b5a33917a3a987
Alexander Kimani <a906k620@ku.edu>
2016-03-31 18:24:16 -0500

    Add method castVote, functionality for all buttons

--- VotingSystem/src/VotingSystem.java
+++ VotingSystem/src/VotingSystem.java
@@ -10,24 +10,25 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 
 public class VotingSystem extends JFrame implements ActionListener
 {
 	private JTextField fName;
 	private JTextField lName;
-	private JLabel midLabel;
 	private JLabel firstName;
-	private JLabel lastName;
 	
-	
+	private Scanner inputStream;
+	private PrintWriter outputStream;
+	private String FileName;
 	public VotingSystem()
 	{
 		
 		
-		//addWindowListener(new WindowDestroyer());
 		setSize(600,300);
 		setTitle("Voting System");
 		Container contentPane = getContentPane();
@@ -35,20 +36,18 @@ public class VotingSystem extends JFrame implements ActionListener
 		
 		
 		JPanel messagePanel = new JPanel();
-		firstName = new JLabel("Enter First Name");
-		lastName = new JLabel("Enter Last Name");
-		midLabel = new JLabel("Choose a candidate");
+		firstName = new JLabel("Enter First Name, Last Name and choose a candidate");
 		messagePanel.add(firstName);
-		messagePanel.add(lastName);
-		//messagePanel.add(topLabel);
+		
 		contentPane.add(messagePanel,BorderLayout.NORTH);
+		
+		
 		
 		JPanel textPanel = new JPanel();
 		fName = new JTextField(15);
 		lName = new JTextField(15);
 		textPanel.add(fName);
 		textPanel.add(lName);
-		//textPanel.add(midLabel,BorderLayout.NORTH);
 		contentPane.add(textPanel, BorderLayout.CENTER);
 		
 		JPanel buttonPanel = new JPanel();
@@ -68,30 +67,77 @@ public class VotingSystem extends JFrame implements ActionListener
 		JButton candidate4 = new JButton("Candidate 4");
 		candidate4.addActionListener(this);
 		buttonPanel.add(candidate4);		
+		
+		JButton reset = new JButton("Reset");
+		reset.addActionListener(this);
+		buttonPanel.add(reset);
 
 		contentPane.add(buttonPanel, BorderLayout.SOUTH);
 	}
 	
+	private void castVote(String Candidate)
+	{
+		try
+		{
+			inputStream = new Scanner(new File(FileName));
+			String line = inputStream.nextLine();
+			firstName.setText("You have already voted for " + line);
+			inputStream.close();
+		}
+		
+		catch(FileNotFoundException e2)
+		{
+			//means havent voted yet so create a file
+			try
+			{
+				outputStream = new PrintWriter(FileName);
+				outputStream.println(Candidate);
+				outputStream.close();
+			}
+			catch(FileNotFoundException e3)
+			{
+				firstName.setText("Error, did not save the file");
+			}
+			firstName.setText("Congratulations for voting");
+		}
+	}
 	
 	public void actionPerformed(ActionEvent e)
 	{
 		String action = e.getActionCommand();
-		if(action.equals("Candidate 1"))
+		FileName = lName.getText() + "_" + fName.getText() + ".txt";
+		if(lName.getText().length() < 1 || fName.getText().length() < 1)
 		{
-			
+			firstName.setText("Must enter your Fist Name and last Name before choosing a Candidate");
+			if(action.equals("Reset"))
+				firstName.setText("Enter First Name, Last Name and choose a candidate");
 		}
-		else if(action.equals("Candidate 2"))
+		else
 		{
-			
+			if(action.equals("Candidate 1"))
+			{
+				castVote("Candidate 1");
+			}
+			else if(action.equals("Candidate 2"))
+			{
+				castVote("Candidate 2");
+			}
+			else if(action.equals("Candidate 3"))
+			{
+				castVote("Candidate 3");
+			}
+			else if(action.equals("Candidate 4"))
+			{
+				castVote("Candidate 4");
+			}
+			else if(action.equals("Reset"))
+			{
+				firstName.setText("Enter First Name, Last Name and choose a candidate");
+				fName.setText("");
+				lName.setText("");
+			}
 		}
-		else if(action.equals("Candidate 3"))
-		{
-			
-		}
-		else if(action.equals("Candidate 4"))
-		{
-			
-		}
+		
 	}
 	public static void main(String[] args)
 	{
