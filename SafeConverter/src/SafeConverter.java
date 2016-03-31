21d5770def3c68c27e07f8d96f669bcea6460739
Alexander Kimani <a906k620@ku.edu>
2016-03-31 00:33:11 -0500

    Assosciate funcionality with buttons

--- SafeConverter/src/SafeConverter.java
+++ SafeConverter/src/SafeConverter.java
@@ -2,29 +2,53 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.Container;
+import java.awt.BorderLayout;
 
-public class SafeConverter 
+class SafeConverter 
 {
 	private JPanel panel;
 	private JButton button;
 	private JButton button2;
 	private JButton button3;
+	private JButton button4;
+	private JButton button5;
+	private JButton button6;
 	private JTextField text;
 	private JLabel label;
+
 	
 	public SafeConverter()
 	{
+		
 		panel = new JPanel();
-		button = new JButton("Convert");
 		text = new JTextField(3);//3 cols, not 20 chars
 		label = new JLabel("Enter a temperature from F to C");
+		button = new JButton("Convert C to F");
+		button2 = new JButton("Convert F to C");
+		button3 = new JButton("Convert C to K");
+		button4 = new JButton("Convert F to K");
+		button5 = new JButton("Convert K to C");
+		button6 = new JButton("Convert K to F");
 		
-		//Load the listener
-		button.addActionListener(buttonListener());	
+		//Load the listeners
+		button.addActionListener(buttonListener());			
+		button2.addActionListener(buttonListener());	
+		button3.addActionListener(buttonListener());	
+		button4.addActionListener(buttonListener());	
+		button5.addActionListener(buttonListener());
+		button6.addActionListener(buttonListener());	
 		
 		//load the panel
 		panel.add(text);
+		
 		panel.add(button);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		panel.add(button5);
+		panel.add(button6);
+		
 		panel.add(label);
 	}
 	
@@ -40,6 +64,8 @@ public class SafeConverter
 			public void actionPerformed(ActionEvent e)
 			{
 				String input = text.getText();
+				String action = e.getActionCommand();
+								
 				
 				String msg = "";
 				if(input.length() < 1)
@@ -50,9 +76,19 @@ public class SafeConverter
 				{
 					try
 					{
-						double tempInF = Double.parseDouble(input);//convert String to double
-						msg = String.format("%.2f",cToK(tempInF));//convert double to String and
-					       //only display 2 places past decimal
+						double temp = Double.parseDouble(input);//convert String to double
+						if(action.equals("Convert C to F"))						
+							msg = String.format("%.2f",cToF(temp));
+						else if(action.equals("Convert F to C"))
+							msg = String.format("%.2f",fToC(temp));
+						else if(action.equals("Convert C to K"))
+							msg = String.format("%.2f",cToK(temp));
+						else if(action.equals("Convert F to K"))
+							msg = String.format("%.2f",fToK(temp));
+						else if(action.equals("Convert K to C"))
+							msg = String.format("%.2f",kToC(temp));
+						else if(action.equals("Convert K to F"))
+							msg = String.format("%.2f",kToF(temp));
 					}
 					catch(Exception exception)
 					{
@@ -92,13 +128,24 @@ public class SafeConverter
 	
 	private double fToK(double temp)
 	{
-		return fToC(temp) + 273.15;
+		return cToK(fToC(temp));
 	}
 	
 	private double cToK(double temp)
 	{
 		return temp + 273.15;
 	}
+	
+	private double kToC(double temp)
+	{
+		return temp - 273.15;
+	}
+	
+	private double  kToF(double temp)
+	{
+		return cToF(kToC(temp));
+	}
+	
 	public static void main(String[] args)
 	{
 		createAndDisplayGUI();
